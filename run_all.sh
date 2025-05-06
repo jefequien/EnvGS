e77ede87cc175e955047899e08fd195528f07ab2
Jeffrey Hu <hujh14@gmail.com>
2025-05-06 10:40:39 +0200

    vids

--- run_all.sh
+++ run_all.sh
@@ -22,18 +22,20 @@ python3 -c "import torch; print(torch.cuda.is_available()); print(torch.cuda.get
 # SCENE_LIST="sedan toycar spheres"
 # TRAJ_NAME="spiral"
 
-# DATASET_NAME="360_v2"
+DATASET_NAME="360_v2"
 # SCENE_LIST="garden bicycle stump bonsai counter kitchen room treehill flowers"
-# TRAJ_NAME="spiral"
+SCENE_LIST="treehill flowers kitchen bicycle stump"
+TRAJ_NAME="spiral"
 
 # DATASET_NAME="neural_catacaustics"
 # SCENE_LIST="compost concave_bowl2 crazy_blade2 hallway_lamp multibounce silver_vase2 wateringcan2"
 # TRAJ_NAME="spiral"
 
-DATASET_NAME="renders"
-# SCENE_LIST="shiny_kitchen shiny_livingroom shiny_office shiny_bedroom"
-SCENE_LIST="multichromeball_kitchen_v2 multichromeball_identical_kitchen_v2 multichromeball_tint_kitchen_v2 multichromeball_value_kitchen_v2"
-TRAJ_NAME="spiral"
+# DATASET_NAME="renders"
+# # SCENE_LIST="shiny_kitchen shiny_livingroom shiny_office shiny_bedroom"
+# # SCENE_LIST="multichromeball_kitchen_v2 multichromeball_identical_kitchen_v2 multichromeball_tint_kitchen_v2 multichromeball_value_kitchen_v2"
+# SCENE_LIST="multichromeball_identical_kitchen_v2 multichromeball_tint_kitchen_v2 multichromeball_value_kitchen_v2"
+# TRAJ_NAME="spiral"
 
 for SCENE in $SCENE_LIST;
 do
@@ -42,13 +44,11 @@ do
     # Train
     evc-train -c configs/exps/envgs/$DATASET_NAME/envgs_$SCENE.yaml exp_name=envgs/$DATASET_NAME/envgs_$SCENE
 
-    # Move checkpoint and render novel views
-    # mkdir -p data/trained_model/envgs_$SCENE
-    # cp data/trained_model/envgs/$DATASET_NAME/envgs_$SCENE/latest.npz  data/trained_model/envgs_$SCENE/latest.npz 
+    # Render novel views
     evc-test -c configs/exps/envgs/$DATASET_NAME/envgs_$SCENE.yaml,configs/specs/$TRAJ_NAME.yaml exp_name=envgs/$DATASET_NAME/envgs_$SCENE
 
     # Saving videos
-    IMAGES_DIR=data/novel_view/envgs_$SCENE
+    IMAGES_DIR=data/novel_view/envgs/$DATASET_NAME/envgs_$SCENE
     ffmpeg -y -framerate 30 -pattern_type glob -i "$IMAGES_DIR/RENDER/*.png"   -c:v libx264 -pix_fmt yuv420p "$IMAGES_DIR/RENDER.mp4"
     ffmpeg -y -framerate 30 -pattern_type glob -i "$IMAGES_DIR/DIFFUSE/*.png"   -c:v libx264 -pix_fmt yuv420p "$IMAGES_DIR/DIFFUSE.mp4"
     ffmpeg -y -framerate 30 -pattern_type glob -i "$IMAGES_DIR/REFLECTION/*.png"   -c:v libx264 -pix_fmt yuv420p "$IMAGES_DIR/REFLECTION.mp4"
